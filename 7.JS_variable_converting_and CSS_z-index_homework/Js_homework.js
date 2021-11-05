7c88bfbb10df3152230fcd40ad07201a41c44eb1
unknown <supervasyl10@gmail.com>
2021-11-05 22:23:47 +0200

    created js task files

---
+++ 7.JS_variable_converting_and CSS_z-index_homework/Js_homework.js
@@ -0,0 +1,31 @@
+let a = 10
+console.log(a)
+console.log(typeof a)
+
+let b = "text"
+console.log(b)
+console.log(typeof b)
+
+let c = true
+console.log(c)
+console.log(typeof c)
+
+let d
+console.log(d)
+console.log(typeof d)
+
+let e = null
+console.log(e)
+console.log(typeof e)
+
+let f = 900719925124740999n
+console.log(f)
+console.log(typeof f)
+
+let value = Symbol('hello');
+console.log(value)
+console.log(typeof value)
+
+let student = { };
+console.log(student)
+console.log(typeof student)
