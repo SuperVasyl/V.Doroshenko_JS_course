58f54ce3ecf77d4de8af7220f9e6607e1323028f
unknown <supervasyl10@gmail.com>
2021-11-18 06:06:36 +0200

    added some code to triangle_two.js file

--- 9.Loops_homework/triangle_two.js
+++ 9.Loops_homework/triangle_two.js
@@ -1,22 +1,7 @@
-let triangle_two = "";
-let y = 11;
-let x = 11;
+let result = ''
 
-            for(let i = 1; i < y; i++ ){
-                for(let j = 1; j < x; j++){
-                    if( j >= i ){
-                        triangle_two = triangle_two.concat("*");
-                    }
-                }
-                triangle_two = triangle_two.concat("\n")
-            }
-            console.log(triangle_two)
-
-
-// https://stackoverflow.com/questions/10993824/do-something-n-times-declarative-syntax
-
-// https://stackoverflow.com/questions/28365737/how-to-print-star-pattern-in-javascript-in-a-very-simple-manner/57166940
-
-// https://www.youtube.com/watch?v=O7E1WXuKh1U
-
-// https://stackoverflow.com/questions/67809323/how-to-print-star-pattern-in-javascript-using-console-log?noredirect=1&lq=1
+for (i = 10; i > 0; i--) {
+    result += '*'
+    .repeat(i)+'\n'
+}
+console.log(result)
