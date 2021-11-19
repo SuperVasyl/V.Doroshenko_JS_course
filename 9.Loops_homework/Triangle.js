d3b1c31db3a70a6ed6e19e2af441e4a9b0fe46de
unknown <supervasyl10@gmail.com>
2021-11-19 06:06:14 +0200

    added new code to triangle.js file

--- 9.Loops_homework/Triangle.js
+++ 9.Loops_homework/Triangle.js
@@ -1,8 +1,19 @@
-let result = ''
+// let result = ''
 
-for (i = 0; i < 11; i++) {
-    result = result +'*'
-    .repeat(i)+'\n'
+// for (i = 0; i < 11; i++) {
+//     result = result +'*'
+//     .repeat(i)+'\n'
+// }
+// console.log(result)
+
+let height = 10
+
+let line = ''
+
+for (i = 0; i < height; i++) {
+    for (j = -1; j < i; j++) {
+        line += '*'
+    }
+    line += '\n'
 }
-console.log(result)
-
+console.log(line)
