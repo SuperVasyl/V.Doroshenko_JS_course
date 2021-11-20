de4bb69565802999aa34c464f9a74e404ae63968
unknown <supervasyl10@gmail.com>
2021-11-20 05:33:02 +0200

    added code to pyramid.js file

--- 9.Loops_homework/pyramid.js
+++ 9.Loops_homework/pyramid.js
@@ -2,3 +2,13 @@ let height = 6;
 
 let line = '';
 
+for (i = 1; i <= height; i++) {
+    for (j = 0; j <= height - i; j++) {
+        line += ' '
+    }
+    for (k = 0; k < 2 * i - 1; k++) {
+        line += '*'
+    }
+    line += '\n'
+}
+console.log(line)
