6b400b336cf1b51f1e3a1418277b4f13e4fb384a
unknown <supervasyl10@gmail.com>
2021-11-21 15:14:25 +0200

    added code and finished rectangle_with_diamond.js file

--- 9.Loops_homework/rectangle_with_diamond.js
+++ 9.Loops_homework/rectangle_with_diamond.js
@@ -1,98 +1,68 @@
-let rectangle = 9
-let length = 13
 let height = 4;
-let string = ''
+let side = 1;
+let hiddenSide = 1;
+let hiddenSide2 = 2
+let row = 13;
+let line = '';
 
-
-for (i = 0; i < rectangle; i++) {
-    for (j = 0; j < length; j++) {
-        if (i === 0 || i === rectangle - 1) {
-            string += '*'
-        } else {
-            if (j === 0 || j === length - 1) {
-                string += '*'
-            } else {
-                // for (i = 1; i <= 4; i++) {
-                //     string += '0'
-                // }
-                // for (i = 1; i <= height; i++) {
-                //     for (j = 0; j <= height - i; j++) {
-                //         string += '0'
-                //     }
-                //     for (k = 0; k < 2 * i - 1; k++) {
-                //         string += '*'
-                //     }
-                //     string += '\n'
-                // }
-                // for (i = 1; i < height; i++) {
-                //     for (j = 0; j <= i; j++) {
-                //         string += ' '
-                //     }
-                //     for (k = (height - i) * 2 - 1; k > 0; k--) {
-                //         string += '*'
-                //     }
-                //     string += '\n'
-                // }
-                string += ' '
-            }
-        }
-    }
-    string += '\n'
+for (i = 0; i < row; i++) {
+  line += '*';
 }
-console.log(string)
+line += '\n';
 
+for (i = 1; i <= height; i++) {
+  for (n = 0; n < side; n++) {
+    line += '*';
+  }
+  for (m = 0; m < hiddenSide; m++) {
+    line += ' ';
+  }
+  for (j = 0; j <= height - i; j++) {
+    line += ' ';
+  }
+  for (k = 0; k < 2 * i - 1; k++) {
+    line += '*';
+  }
+  for (b = 0; b < height - i; b++) {
+      line += ' ';
+  }
+  for (m = 0; m < hiddenSide2; m++) {
+    line += ' ';
+  }
+  for (n = 0; n < side; n++) {
+    line += '*';
+  }
+  line += '\n';
+}
 
+for (i = 1; i < height; i++) {
+  for (n = 0; n < side; n++) {
+    line += '*';
+  }
+  for (m = 0; m < hiddenSide; m++) {
+    line += ' ';
+  }
+  for (j = 0; j <= i; j++) {
+    line += ' ';
+  }
+  for (k = (height - i) * 2 - 1; k > 0; k--) {
+    line += '*';
+  }
+  for (b = 0; b < i; b++) {
+      line += ' ';
+  }
+  for (m = 0; m < hiddenSide2; m++) {
+    line += ' ';
+  }
+  for (n = 0; n < side; n++) {
+    line += '*';
+  }
+  line += '\n';
+}
 
+for (i = 0; i < row; i++) {
+  line += '*';
+}
+line += '\n';
 
-
-
-
-
-
-// let rectangle  = 9
-// let length = 13
-// let string = ''
-
-// for (i = 0; i < rectangle; i++) {
-//     for (j = 0; j < length; j++) {
-//         if (i === 0 || i === rectangle - 1) {
-//             string += '*'
-//         }
-//         else {
-//             if (j === 0 || j === length - 1) {
-//                string += '*' 
-//             }
-//             else {
-//                 string += ' '
-//             }
-//         }
-//     }
-//     string += '\n'
-// }
-// console.log(string)
-
-
-
-// let height = 4;
-// let line = '';
-
-// for (i = 1; i <= height; i++) {
-//     for (j = 0; j <= height - i; j++) {
-//         line += ' '
-//     }
-//     for (k = 0; k < 2 * i - 1; k++) {
-//         line += '*'
-//     }
-//     line += '\n'
-// }
-
-// for (i = 1; i < height; i++) {
-//     for (j = 0; j <= i; j++) {
-//         line += ' '
-//     }
-//     for (k = (height - i) * 2 - 1; k > 0; k--) {
-//         line += '*'
-//     }
-//     line += '\n'
-// }
-// console.log(line)
+console.log(line);
