abe181d9398828609b6acaa7a86900425367da15
unknown <supervasyl10@gmail.com>
2021-11-21 17:55:37 +0200

    added code to giant_star.js file

--- 9.Loops_homework/giant_star.js
+++ 9.Loops_homework/giant_star.js
@@ -1,14 +1,18 @@
 let height = 34;
 
 let line = '';
+let data = [1, 3, 5, 7, 9, 11, 13, 15, 17 , 19 , 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67]
 
 for (i = 1; i <= height; i++) {
     for (j = 0; j <= height - i; j++) {
-        line += ' '
+        line += '0'
     }
     for (k = 0; k < 2 * i - 1; k++) {
         line += '*'
     }
     line += '\n'
 }
+
 console.log(line)
+
+// 2 * i - 1
