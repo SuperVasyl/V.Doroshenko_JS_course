248298c3634d367c01186561fe2e75b86e19f81e
unknown <supervasyl10@gmail.com>
2021-11-24 06:44:17 +0200

    created second task file and code

---
+++ 10.Functions_homework/second_task(multiplication).js
@@ -0,0 +1,32 @@
+//First part for this task. Multiplication for two numbers
+
+function multiplicateTwoNumbers(firstNumber, secondNumber) {
+    return firstNumber * secondNumber
+}
+
+let result = multiplicateTwoNumbers(2, 5);
+console.log(`result for multiplication of TwoNumbers => ${result}`)
+
+alert(`result for multiplication of TwoNumbers => ${result}`)
+
+//second part for this task. Multiplication for three numbers
+
+function multiplicateThreeNumbers(firstNumber, secondNumber, thirdNumber) {
+    return firstNumber * secondNumber * thirdNumber
+}
+
+let result2 = multiplicateThreeNumbers(2, 5, 3);
+console.log(`result for multiplication of three numbers => ${result2}`)
+
+alert(`result for multiplication of three numbers => ${result2}`)
+
+//third part for this task. Exponent
+
+function exponentWithTwoNumbers(firstNumber, secondNumber) {
+    return firstNumber ** secondNumber
+}
+
+let result3 = exponentWithTwoNumbers(2, 5);
+console.log(`result for exponent with two numbers => ${result3}`)
+
+alert(`result for exponent with two numbers => ${result3}`)
