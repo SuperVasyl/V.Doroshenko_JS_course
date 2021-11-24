e2d71566b78421d7ba7af99977be5825d192d221
unknown <supervasyl10@gmail.com>
2021-11-24 06:24:00 +0200

    added code to first task with three numbers

---
+++ 10.Functions_homework/first_task(three_numbers).js
@@ -0,0 +1,8 @@
+function threeNumbers(firstNumber, secondNumber, thirdNumber) {
+    return firstNumber + secondNumber + thirdNumber
+}
+
+let sum = threeNumbers(2, 5, 7);
+console.log('sum for threeNumbers => ' + sum)
+
+alert(`sum for threeNumbers => ${sum}`)
