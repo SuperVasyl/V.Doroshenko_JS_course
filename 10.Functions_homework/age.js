7bef90307fc487951db1ba2a9f7eeac4ca9fb2f0
unknown <supervasyl10@gmail.com>
2021-11-28 22:27:51 +0200

    added expression declaration in code

--- 10.Functions_homework/age.js
+++ 10.Functions_homework/age.js
@@ -12,5 +12,27 @@ function yourAge() {
         alert('Sorry, you are too old for this mess inside')
     } else if (age > 110) {
         alert('Such old people do not exist. Do not lie to me!')
+    } else {
+        alert('Please enter your age in numbers')
     }
 }
+
+//function with Expression
+// let ageWithExpression = function () {
+//     let age = prompt('Please print you age')
+//     if (age <= 18 && age >= 0) {
+//         alert('You are too young for this')
+//     } else if (age < 0) {
+//         alert('Please, do not lie to me')
+//     } else if (age > 18 && age <= 90) {
+//         alert('You are welcome here!')
+//     } else if (age > 90 && age <= 110) {
+//         alert('Sorry, you are too old for this mess inside')
+//     } else if (age > 110) {
+//         alert('Such old people do not exist. Do not lie to me!')
+//     } else {
+//         alert('Please enter your age in numbers')
+//     }
+// }
+
+// ageWithExpression()
