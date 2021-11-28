f36fa868bb2ff802b9b50dd4111608abf4eec2e1
unknown <supervasyl10@gmail.com>
2021-11-28 22:31:34 +0200

    added expression function to this task

--- 10.Functions_homework/SayHi.js
+++ 10.Functions_homework/SayHi.js
@@ -4,3 +4,11 @@ function sayHi() {
     let name = prompt('Say your name, please',)
     alert(`Hello, ${name}`)
 }
+
+// Expression declaration
+// let sayHiwithExpression = function () {
+//     let name = prompt('Say your name, please',)
+//     alert(`Hello, ${name}`)
+// }
+
+// sayHiwithExpression ()
