10573f86eae868e5f898aa0e00f93c3845d1e61c
unknown <supervasyl10@gmail.com>
2021-11-28 22:11:59 +0200

    added expression lines to code

--- 10.Functions_homework/second_task(multiplication).js
+++ 10.Functions_homework/second_task(multiplication).js
@@ -9,6 +9,17 @@ console.log(`result for multiplication of TwoNumbers => ${result}`)
 
 alert(`result for multiplication of TwoNumbers => ${result}`)
 
+//function expression for first task
+let multTwoNumbersExpression = function (fn, sn) {
+    return fn * sn
+}
+
+let resultExp = multTwoNumbersExpression(2, 4);
+console.log('result for multiplication of Two Numbers with expression => ' + resultExp)
+
+alert(`result for multiplication of Two Numbers with expression => ${resultExp}`)
+
+
 //second part for this task. Multiplication for three numbers
 
 function multiplicateThreeNumbers(firstNumber, secondNumber, thirdNumber) {
@@ -20,6 +31,16 @@ console.log(`result for multiplication of three numbers => ${result2}`)
 
 alert(`result for multiplication of three numbers => ${result2}`)
 
+//function expression for second task
+let multThreeNumbersExpression = function (fn, sn, tn) {
+    return fn * sn * tn
+}
+
+let resultExp2 = multThreeNumbersExpression(2, 4, 3);
+console.log('result for multiplication of Three Numbers with expression => ' + resultExp2)
+
+alert(`result for multiplication of Three Numbers with expression => ${resultExp2}`)
+
 //third part for this task. Exponent
 
 function exponentWithTwoNumbers(firstNumber, secondNumber) {
@@ -30,3 +51,13 @@ let result3 = exponentWithTwoNumbers(2, 5);
 console.log(`result for exponent with two numbers => ${result3}`)
 
 alert(`result for exponent with two numbers => ${result3}`)
+
+//function expression for third task
+let exponentTwoNumbersExpression = function (fn, sn) {
+    return fn ** sn
+}
+
+let resultExp3 = exponentTwoNumbersExpression(2, 3);
+console.log('result for exponent of Two Numbers with expression => ' + resultExp3)
+
+alert(`result for exponent of Two Numbers with expression => ${resultExp3}`)
