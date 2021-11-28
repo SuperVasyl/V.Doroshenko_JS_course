dfc198c87f3d7d60aa344649b070f449f6f0aefb
unknown <supervasyl10@gmail.com>
2021-11-28 22:03:25 +0200

    added expression declaration to code

--- 10.Functions_homework/first_task(three_numbers).js
+++ 10.Functions_homework/first_task(three_numbers).js
@@ -1,8 +1,18 @@
-function threeNumbers(firstNumber, secondNumber, thirdNumber) {
+function calcThreeNumbers(firstNumber, secondNumber, thirdNumber) {
     return firstNumber + secondNumber + thirdNumber
 }
 
-let sum = threeNumbers(2, 5, 7);
-console.log('sum for threeNumbers => ' + sum)
+let sum = calcThreeNumbers(2, 5, 7);
+console.log('sum for three Numbers => ' + sum)
 
-alert(`sum for threeNumbers => ${sum}`)
+alert(`sum for three Numbers => ${sum}`)
+
+//function expression
+let calcThreeNumbersExpression = function (fn, sn, tn) {
+    return fn + sn + tn
+}
+
+let sum2 = calcThreeNumbersExpression(1, 4, 8);
+console.log('sum for three Numbers with expression => ' + sum2)
+
+alert(`sum for three Numbers with expression => ${sum2}`)
