f35dffe77924bee0c84bc0e0e1b8e94d680037fa
unknown <supervasyl10@gmail.com>
2021-11-30 20:53:41 +0200

    created first asterlink task with random numbers in prompt

---
+++ 10.Functions_homework/mathRandom.js
@@ -0,0 +1,9 @@
+getRandomNumber()
+
+function getRandomNumber() {
+    let num = Math.ceil(Math.random() * 10);
+    let randNumber = prompt('Guess number between 1 and 10, please', 10)
+    if (randNumber == num) {
+        alert('Matched');
+    } else alert('Not mathed. The number was =>' + num)
+}
