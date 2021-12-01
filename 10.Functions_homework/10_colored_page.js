125b34ab024016c03cdcd723ac819b43e19b2bb6
unknown <supervasyl10@gmail.com>
2021-12-01 23:12:58 +0200

    added code for changing colors in blocks

--- 10.Functions_homework/10_colored_page.js
+++ 10.Functions_homework/10_colored_page.js
@@ -1,13 +1,4 @@
 const button = document.querySelector('button');
-const block = document.querySelector('block');
-
-button.addEventListener('click', () => {
-    let color = '#'
-    color += Math.random().toString(16).slice(2, 8);
-    console.log(color)
-    button.style.backgroundColor = color;
-})
-
 
 // function getRandomColor () {
 //     let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
@@ -17,3 +8,38 @@ button.addEventListener('click', () => {
 // function randomize() {
 //   document.getElementById('block1').style.color = getRandomColor();
 // }
+
+
+button.addEventListener("click", () => {
+    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    console.log(randomColor)
+    let randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor3 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor4 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor5 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor6 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor7 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor8 = Math.floor(Math.random() * 16777215).toString(16);
+    let randomColor9 = Math.floor(Math.random() * 16777215).toString(16);
+    document.getElementById("background").style.backgroundColor = randomColor;
+    document.getElementById("background1").style.backgroundColor = "#" + randomColor1;
+    document.getElementById("background2").style.backgroundColor = "#" + randomColor2;
+    document.getElementById("background3").style.backgroundColor = "#" + randomColor3;
+    document.getElementById("background4").style.backgroundColor = "#" + randomColor4;
+    document.getElementById("background5").style.backgroundColor = "#" + randomColor5;
+    document.getElementById("background6").style.backgroundColor = "#" + randomColor6;
+    document.getElementById("background7").style.backgroundColor = "#" + randomColor7;
+    document.getElementById("background8").style.backgroundColor = "#" + randomColor8;
+    document.getElementById("background9").style.backgroundColor = "#" + randomColor9;
+})
+
+
+
+
+// let string = '';
+// for (i = 0; i < 10; i++) {
+//     string += '#' + Math.floor(Math.random() * 16777215).toString(16);
+//     string += '\n'
+// }
+// console.log(string)
