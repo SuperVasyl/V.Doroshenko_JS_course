7176762d618b3cdaed6a43741af410def8264664
unknown <supervasyl10@gmail.com>
2021-12-03 11:01:52 +0200

    created last task file

---
+++ 11.Objects/cloning_with_recursion.js
@@ -0,0 +1,12 @@
+// https://gist.github.com/djD-REK/e8b1497e7fbf0374e4eada669e5609cf
+
+// https://heyjavascript.com/4-creative-ways-to-clone-objects/
+
+// https://javascript.plainenglish.io/how-to-deep-copy-objects-and-arrays-in-javascript-7c911359b089
+
+// https://medium.com/@Farzad_YZ/3-ways-to-clone-objects-in-javascript-f752d148054d
+
+// https://www.30secondsofcode.org/js/s/deep-clone
+
+// https://blog.logrocket.com/methods-for-deep-cloning-objects-in-javascript/
+
