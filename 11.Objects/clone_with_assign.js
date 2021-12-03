b03697d77b4ba31a8d7aca2b729d79fe3f521c42
unknown <supervasyl10@gmail.com>
2021-12-03 07:00:50 +0200

    created third task homework

---
+++ 11.Objects/clone_with_assign.js
@@ -0,0 +1,34 @@
+let user = {
+    firstName: 'Vasyl',
+    lastName: 'Doroshenko',
+    age: 26,
+    sex: 'male',
+    adress: {
+        country: 'Ucraine',
+        sity: 'L`viv',
+        index: 77415,
+    },
+    education: 'Ucrainian Catolic University',
+    specialty: 'History',
+    workArea: 'ecology',
+    proffecion: 'Garbage seller',
+    hobby: {
+        lazyHobby: 'series',
+        intelectual: 'reading books',
+        active: 'walking in the mountains',
+        service: {
+            firstService: 'for children in Sunday school',
+            secondService: 'for teenagers in Reverse team',
+            thirsServise: 'for the poor and retirees in sity'
+        },
+        fun: 'videogames'
+    },
+    languages: {
+        first: 'Ucrainian',
+        second: 'Russian',
+        third: 'Polish',
+        fourth: 'English',
+        fifth: 'German'
+    },
+    colorOfEyes: 'brown'
+}
