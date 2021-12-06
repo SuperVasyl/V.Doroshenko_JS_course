35b27045d498726ceba599a5f6ab94b8c644b531
unknown <supervasyl10@gmail.com>
2021-12-06 11:44:17 +0200

    added code to 10_colored_page.js

--- 10.Functions_homework/10_colored_page.js
+++ 10.Functions_homework/10_colored_page.js
@@ -1,45 +1,54 @@
-const button = document.querySelector('button');
-
-// function getRandomColor () {
-//     let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-//     return randomColor;
-// }
-
-// function randomize() {
-//   document.getElementById('block1').style.color = getRandomColor();
-// }
-
-
-button.addEventListener("click", () => {
-    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    console.log(randomColor)
-    let randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor3 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor4 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor5 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor6 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor7 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor8 = Math.floor(Math.random() * 16777215).toString(16);
-    let randomColor9 = Math.floor(Math.random() * 16777215).toString(16);
-    document.getElementById("background").style.backgroundColor = randomColor;
-    document.getElementById("background1").style.backgroundColor = "#" + randomColor1;
-    document.getElementById("background2").style.backgroundColor = "#" + randomColor2;
-    document.getElementById("background3").style.backgroundColor = "#" + randomColor3;
-    document.getElementById("background4").style.backgroundColor = "#" + randomColor4;
-    document.getElementById("background5").style.backgroundColor = "#" + randomColor5;
-    document.getElementById("background6").style.backgroundColor = "#" + randomColor6;
-    document.getElementById("background7").style.backgroundColor = "#" + randomColor7;
-    document.getElementById("background8").style.backgroundColor = "#" + randomColor8;
-    document.getElementById("background9").style.backgroundColor = "#" + randomColor9;
-})
+let generateButton = document.querySelector('button');
+// let generateButton = document.getElementById('button'); //generate-btn
+let linearColor = document.getElementById('linear-color'); //utputColor
+let textColorCode = document.getElementById('text-color-code'); //output-code
+let block1 = document.getElementById('block1');
+let block2 = document.getElementById('block2');
+let block3 = document.getElementById('block3');
+let block4 = document.getElementById('block4');
+let block5 = document.getElementById('block5');
+let block6 = document.getElementById('block6');
+let block7 = document.getElementById('block7');
+let block8 = document.getElementById('block8');
+let block9 = document.getElementById('block9');
+let block0 = document.getElementById('block0');
 
 
+let letters = '0123456789ABCDEF';
 
+function getRandomColor() {
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-// let string = '';
-// for (i = 0; i < 10; i++) {
-//     string += '#' + Math.floor(Math.random() * 16777215).toString(16);
-//     string += '\n'
-// }
-// console.log(string)
+let generateBlockColor = () => {
+    let colorOne = getRandomColor();
+    let colorTwo = getRandomColor();
+    let colorThree = getRandomColor();
+    let colorFour = getRandomColor();
+    let colorFive = getRandomColor();
+    let colorSix = getRandomColor();
+    let colorSeven = getRandomColor();
+    let colorEight = getRandomColor();
+    let colorNine = getRandomColor();
+    let colorTen = getRandomColor();
+
+    linearColor.style.background = `linear-gradient(90deg, ${colorOne}, ${colorTwo}, ${colorThree}, ${colorFour}, ${colorFive}, ${colorSix}, ${colorSeven}, ${colorEight}, ${colorNine}, ${colorTen})`;
+
+    block1.style.backgroundColor = `${colorOne}`;
+    block2.style.backgroundColor = `${colorTwo}`;
+    block3.style.backgroundColor = `${colorThree}`;
+    block4.style.backgroundColor = `${colorFour}`;
+    block5.style.backgroundColor = `${colorFive}`;
+    block6.style.backgroundColor = `${colorSix}`;
+    block7.style.backgroundColor = `${colorSeven}`;
+    block8.style.backgroundColor = `${colorEight}`;
+    block9.style.backgroundColor = `${colorNine}`;
+    block0.style.backgroundColor = `${colorTen}`;
+}
+
+generateButton.addEventListener('click', generateBlockColor);
+window.onload = generateBlockColor;
