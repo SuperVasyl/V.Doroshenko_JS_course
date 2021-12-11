35501b24085f388c0cc29a4ecf41bedc27e59451
unknown <supervasyl10@gmail.com>
2021-12-11 12:04:24 +0200

    added new code to this task

--- 12.Arrays/array_with_numbers(third_task).js
+++ 12.Arrays/array_with_numbers(third_task).js
@@ -1,10 +1,8 @@
 const array1 = [1, 3, 5, 3, 2, 6, 3, 11, 15, 3, 27, 45, 3, 47, 76, 3];
 
 function returnArrayWithoutNumber(value) {
-    newArray = array1.filter(function (item) {
-        return item !== value;
-    })
-    return newArray;
+    newArray = array1.filter(item => item !== value)
+    return newArray
 }
 
 console.log(returnArrayWithoutNumber(3));
