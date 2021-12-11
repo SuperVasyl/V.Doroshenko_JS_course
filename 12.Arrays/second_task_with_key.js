9415f9e4499bd6535c6aa086403be78a43705fa2
unknown <supervasyl10@gmail.com>
2021-12-11 11:28:07 +0200

    added some code

--- 12.Arrays/second_task_with_key.js
+++ 12.Arrays/second_task_with_key.js
@@ -1,14 +1,14 @@
-let nameAndAge = [
+const nameAndAge = [
     ['name', 'Vasyl'],
     ['age', 26],
-    ['name', 'Andrew'],
-    ['age', 24],
-    ['name', 'Max'],
-    ['age', 15],
-    ['name', 'Igor'],
-    ['age', 22],
-    ['name', 'Daniel'],
-    ['age', 16]
+    // ['name', 'Andrew'],
+    // ['age', 24],
+    // ['name', 'Max'],
+    // ['age', 15],
+    // ['name', 'Igor'],
+    // ['age', 22],
+    // ['name', 'Daniel'],
+    // ['age', 16]
 ];
 
 console.table(nameAndAge)
@@ -21,13 +21,13 @@ let getNameAndAge = () => {
     }
 }
 
-getNameAndAge()
+// const obj = Object.fromEntries(nameAndAge);
+// console.log(obj);
 
-// for (let i = 0; i < nameAndAge.length; i++) {
-//     // get the size of the inner array
-//     var innerArrayLength = nameAndAge[i].length;
-//     // loop the inner array
-//     for (let j = 0; j < innerArrayLength; j++) {
-//         console.log('[' + i + ',' + j + '] = ' + nameAndAge[i][j]);
+
+// function clonUser() {
+//     for (let objectKey in user) {
+//         console.log(`${objectKey}: ${user[objectKey]}`);
 //     }
 // }
+
