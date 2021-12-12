04a888d41473ed659003d400f4a503d18a2cee89
unknown <supervasyl10@gmail.com>
2021-12-12 18:08:50 +0200

    added code to second asterlink task

--- 12.Arrays/additional_task_two.js
+++ 12.Arrays/additional_task_two.js
@@ -1,3 +1,15 @@
-let array1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21];
+let array1 = [-4, 1, 2, 3, 5, 7, 8, 9, 11, 13, 15, 17, 19, 21];
 
-// https://www.geeksforgeeks.org/given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x/
+function twoSum(arr, S) {
+    var sums = [];
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === S) {
+                sums.push([arr[i], arr[j]]);
+            }
+        }
+    }
+    return sums;
+}
+
+console.log(twoSum(array1, 9));
