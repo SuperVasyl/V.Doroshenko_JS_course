b81cc1c0d6a4885f6ad02b3fdefd110041922664
unknown <supervasyl10@gmail.com>
2021-12-12 19:37:24 +0200

    added another solution to fifth task

--- 12.Arrays/array_with_numbers(fifth_task).js
+++ 12.Arrays/array_with_numbers(fifth_task).js
@@ -5,4 +5,12 @@ const array3 = arr1.concat(arr2);
 function getArrayFromTwoArrays(data) {
     return data.filter((value, index) => data.indexOf(value) !== index);
 }
-console.log(getArrayFromTwoArrays(array3));
+console.log('first solution =>', getArrayFromTwoArrays(array3));
+// another solution
+let array1  = [1, 2, 3, 4, 5, 6];
+let array2 = [1, 3, 4, 5, 7, 8, 9];
+let arr3 = array2.filter(function(obj) { 
+    return array1.indexOf(obj) != -1; 
+});
+
+console.log('second solution =>', arr3);
