625edd13449eb587cfaf8f2d4ae687680e309b09
Vasyl Doroshenko <supervasyl10@gmail.com>
2022-02-12 18:24:50 +0200

    added some Promises to code

--- 14.Async-Promise/index2.js
+++ 14.Async-Promise/index2.js
@@ -27,45 +27,54 @@ console.log(user); //null
 console.log(isError); //false
 
 //Promise
-const getUsersPromise = (userId) => {
-
-}
-
-const p = new Promise(function(resolve, reject) {
-
-})
-//
-const getUserCb = (number, cb) => {
-  if (number > 5) {
-    cb('You have won');
-  } else {
-    cb('less then this number');
-  }
-};
-
-getUserCb(1, (success, error) => {
-  if (error) {
-    console.log('error => ', error);
-  } else {
-    console.log('success => ', success);
-  }
-});
-
-const getUserPromise = (number) => {
-	return new Promise((res, rej) => {
-		getUserCb(1, (success, error) => {
-			if (number > 5) {
-				res('success =>', success);
-			} else {
-				rej('Sad, it is error and less then 5 => ', error);
-			}
-		})
+const getUsersPromise = (userId, name) => {
+	return new Promise((resolve, reject) => {
+		resolve(`userId: ${userId}, name: ${name}`)
+		reject('that was error => ', isError)
 	})
-};
-getUserPromise(8)
-  .then((success) => {
-    console.log('Hey, success => ', success);
+}
+getUsersPromise('Users_202', 'Andrew')
+  .then((data) => {
+    console.log('Some data =>', data);
   })
-  .catch((error) => {
-    console.log('Oh, error => ', error);
+  .catch((isError) => {
+    console.log('Oh, error => ', isError);
   });
+
+
+
+//
+// const getUserCb = (number, cb) => {
+//   if (number > 5) {
+//     cb('You have won');
+//   } else {
+//     cb('less then this number');
+//   }
+// };
+
+// getUserCb(1, (success, error) => {
+//   if (error) {
+//     console.log('error => ', error);
+//   } else {
+//     console.log('success => ', success);
+//   }
+// });
+
+// const getUserPromise = (number) => {
+// 	return new Promise((res, rej) => {
+// 		getUserCb(1, (success, error) => {
+// 			if (number > 5) {
+// 				res('success =>', success);
+// 			} else {
+// 				rej('Sad, it is error and less then 5 => ', error);
+// 			}
+// 		})
+// 	})
+// };
+// getUserPromise(8)
+//   .then((success) => {
+//     console.log('Hey, success => ', success);
+//   })
+//   .catch((error) => {
+//     console.log('Oh, error => ', error);
+//   });
---
+++ 15.HTTP-JSON/index.js
@@ -0,0 +1,6 @@
+
+
+// https://www.youtube.com/watch?v=Tr82mUcLNO0&list=PLvaxnpPm_Z5LFxlTOETFpWAjxWueMiMb3&index=9
+// https://docs.google.com/document/d/1GYB7jT0vCeNm7C9WI02FkL9-85VdxfZaA-UP4VcWksM/edit
+
+// http://old.code.mu/tasks/javascript/base/osnovy-yazyka-javascript-dlya-novichkov.html
