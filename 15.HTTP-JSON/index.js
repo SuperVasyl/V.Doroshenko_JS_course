c42b8448f0f964a3e9f40bb29b25fde164dbe575
Vasyl Doroshenko <supervasyl10@gmail.com>
2022-02-15 20:25:28 +0200

    added code during the lesson lection for test and learning

--- 15.HTTP-JSON/index.js
+++ 15.HTTP-JSON/index.js
@@ -1,6 +1,46 @@
 
+// let url = 'https://api.monobank.ua/bank/currency';
+// let response = fetch(url);
+// response.then(value => console.log(value));
+// console.log(response);
 
-// https://www.youtube.com/watch?v=Tr82mUcLNO0&list=PLvaxnpPm_Z5LFxlTOETFpWAjxWueMiMb3&index=9
-// https://docs.google.com/document/d/1GYB7jT0vCeNm7C9WI02FkL9-85VdxfZaA-UP4VcWksM/edit
+let baseUrl = 'https://jsonplaceholder.typicode.com';
+
+// const getUsers = async () => {
+// 	let url = `${baseUrl}/users`;
+// 	let response = await fetch(url);
+// 	let json = await response.json();
+// 	console.log(response);
+// 	console.log(json); 
+// }
+
+const getUsers = async () => {
+  let url = `${baseUrl}/users`;
+	let body = {
+		id: '18481861',
+		name: 'Vasyl',
+		username: 'Doroshenko',
+	}
+	let bodyJson = JSON.stringify(body);
+
+  let response = await fetch(url, {
+		method: 'POST',
+		body: bodyJson,
+	});
+
+  let json = await response.json();
+  // console.log(response);
+  console.log(json);
+};
+
+getUsers();
+
+
+
+// https://www.youtube.com/watch?v=8OgQdlQzA-U&list=PLvaxnpPm_Z5LFxlTOETFpWAjxWueMiMb3&index=8
+
+// https://developer.mozilla.org/ru/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data
+
+// https://docs.google.com/document/d/1ApQzDtOEe-dxjrA7BjGJpaFjIzj__CzE_sKbX6A1APw/edit
 
 // http://old.code.mu/tasks/javascript/base/osnovy-yazyka-javascript-dlya-novichkov.html
