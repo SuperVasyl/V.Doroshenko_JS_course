3a4af7ff437b4336be88d2bbb4a8a725f0ba93ce
Vasyl Doroshenko <supervasyl10@gmail.com>
2022-02-20 13:01:59 +0200

    created started angular project

---
+++ 19.Angular/projectHomeWork/src/app/components/parent/parent.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.scss']
+})
+export class ParentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+// https://www.youtube.com/watch?v=NnZub6glxXc&list=PLvaxnpPm_Z5LFxlTOETFpWAjxWueMiMb3&index=2
